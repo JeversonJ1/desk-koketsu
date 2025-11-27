dffc07db17de1e9fbd110f8938555b21e024d972
JeversonJ1 <jeversonbrito@outlook.com>
2025-11-27 11:54:51 -0300

    iniciando o desktop

--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,6 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { contextBridge, ipcRenderer } from "electron";
+
+contextBridge.exposeInMainWorld("api", {
+  criarProduto: (dados) => ipcRenderer.invoke("produto:criar", dados),
+  listarProdutos: () => ipcRenderer.invoke("produto:listar")
+});
