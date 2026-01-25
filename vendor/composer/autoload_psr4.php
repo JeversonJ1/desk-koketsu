b576e95c622cff5591b386028993b18316be223f
jeverson <jeversonbrito@outlook.com>
2026-01-25 16:16:05 -0300

    refactor: ajustar formatação e organização dos arquivos de configuração do Vite

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,6 +6,9 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
+    'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
+    'PHPMailer\\PHPMailer\\' => array($vendorDir . '/phpmailer/phpmailer/src'),
+    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
     'App\\Koketsu\\Controles\\' => array($baseDir . '/backend/app/controllers'),
     'App\\Koketsu\\' => array($baseDir . '/backend'),
 );
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitc50a4d162b96fcea21cfac123a6bd42e
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitc50a4d162b96fcea21cfac123a6bd42e', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitc50a4d162b96fcea21cfac123a6bd42e', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInitc50a4d162b96fcea21cfac123a6bd42e
         call_user_func(\Composer\Autoload\ComposerStaticInitc50a4d162b96fcea21cfac123a6bd42e::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitc50a4d162b96fcea21cfac123a6bd42e::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
